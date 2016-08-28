c6562da726b03841012d8c2eecb7029729592840
rat395 <rat395@ymail.com>
2016-08-28 10:01:33 -0700

    changed the second for loop so that invader2b appears

--- LED_MATRIX_CLUB_RUSH.ino
+++ LED_MATRIX_CLUB_RUSH.ino
@@ -66,10 +66,10 @@ void loop() {
 
   
   for (int i = 0; i < 8; i++) {
-      if (i % 1 == 0) {
-          printByte(invader2a);
-        } else {
+      if (i % 2 == 0) {
           printByte(invader2b);
+        } else {
+          printByte(invader2a);
         }
 
       delay(1200);
